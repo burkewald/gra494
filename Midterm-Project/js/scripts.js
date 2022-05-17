105fbae4948f6075c38462d5e2f26165a7fbe74a
burkewald <70238524+burkewald@users.noreply.github.com>
2022-05-16 18:57:18 -0700

    Add files via upload

--- Midterm-Project/js/scripts.js
+++ Midterm-Project/js/scripts.js
@@ -1,4 +1,3 @@
-//Display of the Navigation on mobile//
 function myFunction() {
 var x = document.getElementById("myLinks");
 var y = document.getElementById("bars");
@@ -9,27 +8,3 @@ if (x.style.display === "block") {
   x.style.display = "block";
   y.classList.add("active");
 }}
-
-
-function myFunction() {
-var x = document.getElementById("extrainfo");
-if (x.style.display === "none") {
-  x.style.display = "block";
-} else {
-  x.style.display = "none";
-}}
-
-
-
-
-    Array.from(document.querySelectorAll('.video-link-wrap'), function (elem) {
-        elem.addEventListener('click', function hideContent(e) {
-            e.currentTarget.nextElementSibling.classList.toggle('hidden');
-        });
-    });
-    Array.from(document.querySelectorAll('.video-overlay .close'), function (elem) {
-        elem.addEventListener('click', function hideContent(e) {
-            e.currentTarget.parentElement.classList.toggle('hidden');
-        });
-    });
-  
